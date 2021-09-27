1f9d7500dfb3692a678fde9c999b5ad9562e6d08
Abhishek Dhillon <abhishek.dhillon@paxcel.net>
2021-09-27 11:39:33 +0530

    first commit

---
+++ angular-12-jwt-authentication/src/app/forgot-password/forgot-password.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { TokenStorageService } from '../_services/token-storage.service';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent implements OnInit {
+  form: any = {
+    email: null
+  };
+  isLoggedIn = false;
+  roles: string[] = [];
+  isLoginFailed = false;
+  errorMessage = '';
+  responseMessage = ''
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+
+  ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getUser().roles;
+    }
+  }
+
+  onSubmit(): void {
+    const { email } = this.form;
+
+    this.authService.forgotPassword( email ).subscribe((data) => {
+        console.log(data);
+        var temp = data;
+        this.responseMessage = temp["message"]
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    );
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
+
+}
